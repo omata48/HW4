659a061977df116824169f01104df2e218869787
Omar <62365988+omata48@users.noreply.github.com>
2020-06-23 19:54:40 -0500

    finished score logic for app and updated style

--- javascript/score.js
+++ javascript/score.js
@@ -4,36 +4,52 @@ var clearScoresBtn = document.getElementById("clear-scores");
 var scoresContainer = document.getElementById("scores-container");
 
 var highScores = [];
+var newScore;
+
+function init() {
+  // Write code here to check if there are todos in localStorage
+  // If so, parse the value from localStorage and assign it to the todos variable
+  newScore = JSON.parse(localStorage.getItem("user-score"));
+  highScores = JSON.parse(localStorage.getItem("high-scores"));
+  // first time loading page, will return null, can set array to start
+  if (highScores === null){
+    highScores = []
+  }
+  // console.log(newScore);
+  if (newScore !== null){
+    highScores.push(newScore);
+    localStorage.removeItem("user-score");
+  }
+  // Render scores to the DOM
+  renderScores();
+}
 
 function renderScores() {
   // Clear todoList element and update todoCountSpan
   scoresContainer.innerHTML = "";
-
+  // console.log(highScores.length);
+  // sort Scores by highest on index 0 before rendering
+  if (highScores.length > 1){
+    highScores = sortScores(highScores);
+  }
   // Render a new li for each todo
   for (var i = 0; i < highScores.length; i++) {
     var currentUser = highScores[i];
     var currentInitials = currentUser.initials;
     var currentScore = currentUser.finalScore;
-    console.log(currentInitials);
-    console.log(currentScore);
+    // console.log(currentInitials);
+    // console.log(currentScore);
 
     var pEl = document.createElement("p");
-    pEl.textContent = currentInitials + " - " + currentScore;
+    pEl.textContent = (i+1) + ". " + currentInitials + " - " + currentScore;
     pEl.setAttribute("data-index", i);
-    pEl.className = "bg-secondary text-white"
+    pEl.className = "bg-secondary text-white scores"
     scoresContainer.appendChild(pEl);
   }
-}
-
-function init() {
-  // Write code here to check if there are todos in localStorage
-  // If so, parse the value from localStorage and assign it to the todos variable
-  var startingScores = JSON.parse(localStorage.getItem("user-score"));
-  console.log(startingScores);
-  highScores = [startingScores];
-  console.log(highScores);
-  // Render todos to the DOM
-  renderScores();
+  storeScores(highScores);
+  // console.log(highScores);
+  // var score1 = highScores[0].finalScore;
+  // console.log(score1);
 }
 
 function storeScores(listObj) {
@@ -41,17 +57,36 @@ function storeScores(listObj) {
   localStorage.setItem("high-scores",JSON.stringify(listObj));
 }
 
+function sortScores(scoresArray){
+  // will not run unless at least length 2
+  // needs to go from bottom to top
+  // console.log("Original:");
+  // console.log(scoresArray);
+  // scoresArray = scoresArray.reverse();
+  // console.log("Reverse:");
+  // console.log(scoresArray);
+  var sortedArray = scoresArray;
+  var arrayLength = scoresArray.length-1;
+  var swapped;
+  do {
+    swapped = false
+    for (var i = 0;i < arrayLength;i++){
+      if ((scoresArray[i].finalScore) < (scoresArray[i+1].finalScore)){
+        var temp  = sortedArray[i];
+        sortedArray[i] = sortedArray[i+1];
+        sortedArray[i+1] = temp;
+        swapped = true;
+      }
+    } 
+  
+  } while(swapped);
+  return sortedArray;
+}
+
 // remove with complete button
-clearScoresBtn.addEventListener("click", function(event){
-  var element = event.target
-  if (element.matches("button")){
-    console.log("click");
-    var index = parseInt(element.parentElement.getAttribute("data-index"));
-    todos.splice(index, 1);
-    storeTodos(todos);
-    renderTodos();
-  }
+clearScoresBtn.addEventListener("click", function(){
+  localStorage.removeItem("high-scores");
+  init();
 })
 
 init();
-renderScores();
